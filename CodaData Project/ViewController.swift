f774138fab870b096b43f01aeddc4c9a65808e2b
sakshi.jaiswal <sakshi.jaiswal@actolap.com>
2020-03-02 19:57:40 +0530

    updated code

--- CodaData Project/ViewController.swift
+++ CodaData Project/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txt_name: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

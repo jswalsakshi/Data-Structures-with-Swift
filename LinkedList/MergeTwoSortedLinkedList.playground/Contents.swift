a262df51347748a2915975ea5bae4ee303cfca72
Sakshi Jaiswal <sakshi.jaiswal@1mg.com>
2021-05-24 08:46:59 +0530

    recursive way

--- LinkedList/MergeTwoSortedLinkedList.playground/Contents.swift
+++ LinkedList/MergeTwoSortedLinkedList.playground/Contents.swift
@@ -10,7 +10,7 @@ public class ListNode {
 }
 
 class Solution {
-  func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+  func mergeTwoListsIterative(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
     var list1 = l1
     var list2 = l2
     var sortedList = ListNode()
@@ -58,5 +58,21 @@ class Solution {
     }
     
     return headNode
+  }
+  
+  func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+    if(l1 == nil) {return l2}
+    if(l2 == nil) {return l1}
+    var result:ListNode?
+    
+    if(l1!.val <= l2!.val) {
+      result = l1
+      result!.next = mergeTwoLists(l1!.next,l2)
+    }
+    else {
+      result = l2
+      result!.next = mergeTwoLists(l1,l2!.next)
+    }
+    return result
   }
 }

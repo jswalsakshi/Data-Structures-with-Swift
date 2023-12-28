e6313518f7c9a89dc1f760fc778297b51f8cc6fa
jswalsakshi <my@pers.on.al>
2023-12-28 10:41:38 +0530

    Added MCM code in swift

--- MatrixChainMultiplication.playground/Contents.swift
+++ MatrixChainMultiplication.playground/Contents.swift
@@ -24,3 +24,28 @@ func MCM(arr: [Int], i: Int, j: Int) -> Int {
 let arr = [40, 20, 30, 10, 30]
 
 print(MCM(arr: arr, i: 1, j: arr.count - 1))
+
+
+func minNumOfPartition(s: String, i: Int, j: Int) -> Int {
+    if(i>=j) {
+        return 0
+    }
+    
+    var tempAns: Int
+    var mn = Int.max
+    
+    for k in i...j {
+        tempAns = (minNumOfPartition(s: s, i: i, j: k) +
+                  minNumOfPartition(s: s, i: k+1, j: j) + 1)
+        
+        if tempAns < mn {
+            mn = tempAns
+        }
+    }
+    
+    return mn
+}
+
+let s = "nitik"
+
+print(minNumOfPartition(s: s, i: 0, j: s.count - 1))
